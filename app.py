1267f3aae3ace46aa0949ced5c0bf686ac39c65b
IsaGarciaSense <isa@sense-ai.co>
2024-06-04 01:27:34 -0500

    se creo la api

--- app.py
+++ app.py
@@ -1,26 +1,53 @@
 from flask import Flask, request, jsonify, send_from_directory
 import pickle
 import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
 # Carga el modelo
-with open('C:\\Users\sense\\Infomedica\\model\\training_model.pkl', 'rb') as f:
+with open('model/training_model.pkl', 'rb') as f:
     modelo = pickle.load(f)
 
+# Función para preprocesar los datos del paciente
+def preprocess_data(df):
+    # Eliminar columnas no necesarias
+    df = df.drop(columns=['COD','Año','Mes','Paciente Tipo Identificacion',  'Nro Atencion', 'Paciente Entidad Responsable Pago',
+                             'Paciente Modalidad Contrato','Paciente Regimen Afiliacion', 'Urg Fecha (Camara)','Urg Fecha Ingreso',
+                             'Urg Fecha Triaje','Urg Fecha Consulta F3' , 'Dias (Camara - F3)','Horas (Camara - F3)',
+                             'Urg Demora1 Consulta (Minutos)', 'Dias (Camara - Triaje)', 'Urg Demora Triaje (Minutos)', 'Dias (Triaje - F3)',
+                             'Horas (Triaje - F3)','Urg Demora2 Consulta (Minutos)','Profesional Especialidad','Dx Principal Tipo ',
+                             'Dx Principal', 'Dx Principal Capitulo', 'Dx Principal.1','Dx Relacionado1','Dx Relacionado2','Dx Relacionado3', 'Paciente Edad',
+                             'Unidad', 'Grupo Edad', 'Paciente Sexo',  'Profesional Identificacion', 'Dias', 'Horas', 'Grupo Poblacional', 'Pertenencia Etnica',
+                             'ALTO COSTO'])
+
+    # Codificar las columnas categóricas usando One-Hot Encoding
+    df = pd.get_dummies(df)
+
+    return df
+
+# Definir una ruta para hacer predicciones
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json(force=True)
-    prediccion = modelo.predict(np.array(data['input']).reshape(1, -1))
-    #prediccion = modelo.predict(np.array([[6.4, 3.1, 5.5]]))
-    #array([[6.4, 3.1, 5.5]])
-    output = prediccion[0]
-    output = int(output)
-    return jsonify({'prediction': output})
+    # Obtener el archivo Excel del cuerpo de la solicitud
+    file = request.files['file']
+
+    # Cargar el archivo Excel en un DataFrame
+    df_paciente = pd.read_excel(file)
+
+    # Preprocesar los datos del paciente
+    X_new = preprocess_data(df_paciente)
+
+    # Hacer la predicción con el modelo
+    prediction = modelo.predict(X_new)
+
+    # Devolver la predicción como JSON
+    return jsonify({'prediction': prediction.tolist()})
 
 @app.route('/')
 def serve_prediction_page():
     return send_from_directory('static', 'prediction.html')
 
+# Ejecutar la aplicación Flask
 if __name__ == '__main__':
     app.run(debug=True)
