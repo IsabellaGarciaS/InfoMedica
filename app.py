442160c3acff7dd515712541967a0e807bcfbd1f
IsaGarciaSense <isa@sense-ai.co>
2024-06-04 13:34:41 -0500

    Se corrigió un error de l guardado del modelo

--- app.py
+++ app.py
@@ -6,20 +6,20 @@ import pandas as pd
 app = Flask(__name__)
 
 # Carga el modelo
-with open('model/training_model.pkl', 'rb') as f:
+with open('model/best_model.pkl', 'rb') as f:
     modelo = pickle.load(f)
 
 # Función para preprocesar los datos del paciente
 def preprocess_data(df):
     # Eliminar columnas no necesarias
-    df = df.drop(columns=['COD','Año','Mes','Paciente Tipo Identificacion',  'Nro Atencion', 'Paciente Entidad Responsable Pago',
-                             'Paciente Modalidad Contrato','Paciente Regimen Afiliacion', 'Urg Fecha (Camara)','Urg Fecha Ingreso',
-                             'Urg Fecha Triaje','Urg Fecha Consulta F3' , 'Dias (Camara - F3)','Horas (Camara - F3)',
-                             'Urg Demora1 Consulta (Minutos)', 'Dias (Camara - Triaje)', 'Urg Demora Triaje (Minutos)', 'Dias (Triaje - F3)',
-                             'Horas (Triaje - F3)','Urg Demora2 Consulta (Minutos)','Profesional Especialidad','Dx Principal Tipo ',
-                             'Dx Principal', 'Dx Principal Capitulo', 'Dx Principal.1','Dx Relacionado1','Dx Relacionado2','Dx Relacionado3', 'Paciente Edad',
-                             'Unidad', 'Grupo Edad', 'Paciente Sexo',  'Profesional Identificacion', 'Dias', 'Horas', 'Grupo Poblacional', 'Pertenencia Etnica',
-                             'ALTO COSTO'])
+    # df = df.drop(columns=['COD','Año','Mes','Paciente Tipo Identificacion',  'Nro Atencion', 'Paciente Entidad Responsable Pago',
+    #                          'Paciente Modalidad Contrato','Paciente Regimen Afiliacion', 'Urg Fecha (Camara)','Urg Fecha Ingreso',
+    #                          'Urg Fecha Triaje','Urg Fecha Consulta F3' , 'Dias (Camara - F3)','Horas (Camara - F3)',
+    #                          'Urg Demora1 Consulta (Minutos)', 'Dias (Camara - Triaje)', 'Urg Demora Triaje (Minutos)', 'Dias (Triaje - F3)',
+    #                          'Horas (Triaje - F3)','Urg Demora2 Consulta (Minutos)','Profesional Especialidad','Dx Principal Tipo ',
+    #                          'Dx Principal', 'Dx Principal Capitulo', 'Dx Principal.1','Dx Relacionado1','Dx Relacionado2','Dx Relacionado3', 'Paciente Edad',
+    #                          'Unidad', 'Grupo Edad', 'Paciente Sexo',  'Profesional Identificacion', 'Dias', 'Horas', 'Grupo Poblacional', 'Pertenencia Etnica',
+    #                          'ALTO COSTO'])
 
     # Codificar las columnas categóricas usando One-Hot Encoding
     df = pd.get_dummies(df)
@@ -40,9 +40,11 @@ def predict():
 
     # Hacer la predicción con el modelo
     prediction = modelo.predict(X_new)
+    output=prediction[0]
+    output=str(output)
 
     # Devolver la predicción como JSON
-    return jsonify({'prediction': prediction.tolist()})
+    return jsonify({'prediction': output})
 
 @app.route('/')
 def serve_prediction_page():
